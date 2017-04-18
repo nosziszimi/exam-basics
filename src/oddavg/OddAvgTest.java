36e2f7ed82237c589af03b7372ad8c33541173ad
Aradi Nóra <aradinora111@gmail.com>
2017-04-18 10:38:50 +0200

    OddAvg - recreate OddAvgTest class and add first test

--- src/oddavg/OddAvgTest.java
+++ src/oddavg/OddAvgTest.java
@@ -1,8 +1,22 @@
 package oddavg;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
 /**
  * Created by Nóra on 2017. 04. 18..
  */
-public class OddAvgTest {
+class OddAvgTest {
+
+  OddAvg oddAvg = new OddAvg();
+
+  @Test
+  void oddAverage() {
+    ArrayList<Integer> listOfNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+    assertEquals(4, oddAvg.oddAverage(listOfNumbers));
+  }
 
 }
