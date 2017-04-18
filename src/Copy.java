3a9dd1a74906cd68e60edb06ab3faffcd9705eee
Aradi Nóra <aradinora111@gmail.com>
2017-04-18 11:42:15 +0200

    Copy - add readAndWrite() method and create the reading part in it

--- src/Copy.java
+++ src/Copy.java
@@ -1,4 +1,10 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Nóra on 2017. 04. 18..
@@ -16,5 +22,15 @@ public class Copy {
   public static boolean isFile(String path) {
     File file = new File(path);
     return file.isFile();
+  }
+
+  public static void readAndWrite(String sourcePath, String destinationPath) {
+    Path source = Paths.get(sourcePath);
+    try {
+      List<String> lines = new ArrayList<>(Files.readAllLines(source));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
   }
 }
