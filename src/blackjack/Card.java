cbcfb4fbed9daad326f818fceca9525dfe8635fd
Aradi Nóra <aradinora111@gmail.com>
2017-04-18 12:59:28 +0200

    BlackJack - create final fields COLORS and VALUES in the Card class

--- src/blackjack/Card.java
+++ src/blackjack/Card.java
@@ -1,10 +1,15 @@
 package blackjack;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 /**
  * Created by Nóra on 2017. 04. 18..
  */
 public class Card {
-
+  static final HashSet<String> COLORS = new HashSet<>(Arrays.asList("Clubs", "Diamonds", "Hearts", "Spades"));
+  static final ArrayList VALUES = new ArrayList<>(Arrays.asList("Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"));
   String color, value;
 
   public Card(String color, String value) {
