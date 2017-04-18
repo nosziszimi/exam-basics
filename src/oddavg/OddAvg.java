c749b799138dfb18b774a02cf431aa53b649a47f
Aradi Nóra <aradinora111@gmail.com>
2017-04-18 10:03:14 +0200

    OddAvg - create oddAverage() function

--- src/oddavg/OddAvg.java
+++ src/oddavg/OddAvg.java
@@ -1,5 +1,7 @@
 package oddavg;
 
+import java.util.ArrayList;
+
 /**
  * Created by Nóra on 2017. 04. 18..
  */
@@ -9,6 +11,21 @@ public class OddAvg {
     // Create a function called `oddAverage` that takes a list of numbers as parameter
     // and returns the average value of the odd numbers in the list
     // Create basic unit tests for it with at least 3 different test cases
+  }
+
+  public static int oddAverage(ArrayList<Integer> listOfNumbers) {
+    ArrayList<Integer> oddNumbers = new ArrayList<>();
+    for (Integer number : listOfNumbers) {
+      if (number % 2 == 1) {
+        oddNumbers.add(number);
+      }
+    }
+    int oddAvg = 0;
+    for (Integer oddNumber : oddNumbers) {
+      oddAvg += oddNumber;
+    }
+    oddAvg /= oddNumbers.size();
+    return oddAvg;
   }
 
 }
