ef31bd52abd9e6b41dc483af74d781b1917ab983
Aradi Nóra <aradinora111@gmail.com>
2017-04-18 13:16:51 +0200

    BlackJack - create random color generating part of the fillCardsList() method

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -16,17 +16,33 @@ public class Deck {
   }
 
   public void fillCardsList(int numberOfCards) {
+    int cardsNumber = numberOfCards;
     ArrayList<String> colors = new ArrayList<>();
     ArrayList<String[]> allCardColorsAndValues = new ArrayList<>();
-    if (numberOfCards < 5) {
+    if (numberOfCards < 4) {
       for (String color : Card.COLORS) {
-        String cardColor = new String ("color");
+        String cardColor = new String("color");
         colors.add(cardColor);
       }
       for (int i = 0; i < numberOfCards; i++) {
         String[] colorAndValue = new String[2];
-        colorAndValue[0] = colors.get(i);;
+        colorAndValue[0] = colors.get(i);
         allCardColorsAndValues.add(colorAndValue);
+      }
+    } else {
+      do {
+        for (String color : Card.COLORS) {
+          String[] colorAndValue = new String[2];
+          colorAndValue[0] = color;
+          allCardColorsAndValues.add(colorAndValue);
+          cardsNumber -= Card.COLORS.size();
+        }
+      } while (numberOfCards != 0); {
+        int random = (int)(Math.random() * colors.size());
+        String[] colorAndValue = new String[2];
+        colorAndValue[0] = colors.get(random);
+        allCardColorsAndValues.add(colorAndValue);
+        cardsNumber -= 1;
       }
     }
   }
